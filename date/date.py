1ceb55b03d665619afa4d0a5a636d1bdf0582d86
Smruthi Elapavuluri <jsmruthi@gmail.com>
2017-02-13 11:41:46 -0800

    First commit- Python Packaging

---
+++ date/date.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import datetime
+
+
+
+def printDate():
+ today = datetime.date.today()
+ print today
---
+++ hello.py
@@ -0,0 +1,7 @@
+import os
+import sys
+from date import date
+
+print "Todays date is:"
+date.printDate()
+
